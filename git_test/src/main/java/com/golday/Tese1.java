0cfbf92d141b58f15044baea351a93f402f1fd4e
强 <782151846@qq.com>
2019-10-15 09:04:12 +0800

    第er次使用idea

--- git_test/src/main/java/com/golday/Tese1.java
+++ git_test/src/main/java/com/golday/Tese1.java
@@ -3,5 +3,7 @@ package com.golday;
 public class Tese1 {
     public static void main(String[] args) {
         System.out.println("第一次测试...");
+        System.out.println("第二次测试...");
+        System.out.println("第二次测试...");
     }
 }
